69402f52e0634e2f2349579beb5cebd2b6ba3d9d
ogtayhuseynov0 <ogtayhuseynov0@gmail.com>
2023-12-08 01:35:38 +0400

    r

---
+++ src/d1p2.js
@@ -0,0 +1,37 @@
+import fs from 'fs'
+
+const nums = {
+  'one': 1,
+  'two': 2,
+  'three': 3,
+  'four': 4,
+  'five': 5,
+  'six': 6,
+  'seven': 7,
+  'eight': 8,
+  'nine': 9
+}
+
+const content = fs.readFileSync('./inputs/d1p2', 'utf8').split('\n').filter(x => x)
+const numbers = content.map((line, idx) => {
+  const nms = []
+  for (let i = 0; i < line.length; i++) {
+    if (!isNaN(parseInt(line[i]))) {
+      nms.push(line[i])
+      ++i;
+    }
+    Object.keys(nums).forEach(key => {
+      const va = line.substr(i, key.length)
+      if (va == key) {
+        nms.push(nums[key])
+      }
+    })
+
+  }
+
+  const [a, b] = [nms[0], nms[nms.length - 1]]
+  console.log(idx, nms, a.toString(), b.toString())
+  return a.toString() + b.toString()
+})
+const res = numbers.reduce((acc, i) => acc + parseInt(i), 0)
+console.log(res)
