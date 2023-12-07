e854b1f5dade5c1dd6c1ed3d34943e4a2487580c
ogtayhuseynov0 <ogtayhuseynov0@gmail.com>
2023-12-08 00:06:00 +0400

    day one finished part 1

--- src/d1p1.js
+++ src/d1p1.js
@@ -1,5 +1,9 @@
 import fs from 'fs'
 
-const content = fs.readFileSync('./inputs/d1p1', 'utf8').split('\n')
-
-console.log(content)
+const content = fs.readFileSync('./inputs/d1p1', 'utf8').split('\n').filter(x => x)
+const numbers = content.map(line => {
+  const res = line.replace(/[a-z]/g, '').split('')
+  const [a, b] = [res[0], res[res.length - 1]]
+  return a + b
+}).reduce((acc, i) => acc + parseInt(i), 0)
+console.log(numbers)
